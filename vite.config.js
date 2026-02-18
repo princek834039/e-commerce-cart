ad2f597671eeb95f32e308ffc85b7df03d0a89b9
Prince Kumar <princekumar.peoplepro@gmail.com>
2026-02-18 16:15:16 +0530

    Change base URL for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://princek834039.github.io/e-commerce-cart/', 
+  base: '/e-commerce-cart/', 
   plugins: [
     react({
       babel: {

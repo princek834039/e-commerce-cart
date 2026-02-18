2415fa691495d7fb91c65f02a48d277f1317b4e9
Prince Kumar <princekumar.peoplepro@gmail.com>
2026-02-18 16:48:38 +0530

    In Package and vite.configure

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/e-commerce-cart/', 
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: '/e-commerce-cart/',
+  plugins: [react()],
 })
